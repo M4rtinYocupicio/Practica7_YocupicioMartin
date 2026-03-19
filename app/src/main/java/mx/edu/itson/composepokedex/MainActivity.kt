bc34fead3e8868195136c0b6cb0bd41cb044c4f3
M4rtinYocupicio <martin.yocupicio228307@potros.itson.edu.mx>
2026-03-19 01:28:28 -0700

    Se agregó el PokemonHeader y parte de PokemonCard

--- app/src/main/java/mx/edu/itson/composepokedex/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/composepokedex/MainActivity.kt
@@ -1,19 +1,40 @@
 package mx.edu.itson.composepokedex
 
+import android.R.attr.contentDescription
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import mx.edu.itson.composepokedex.domain.Pokemon
 import mx.edu.itson.composepokedex.ui.theme.ComposepokedexTheme
 
 class MainActivity : ComponentActivity() {
+    val pokemon = Pokemon("Pikachu", 25, "Electrico", "Es una rata electrica",
+        0.4f, 6f, true, "Estatica", R.drawable.pikachu)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,12 +42,40 @@ class MainActivity : ComponentActivity() {
             ComposepokedexTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        pokemon,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun PokemonHeader(name:String, number:Int, fav: Boolean){
+    Row(Modifier.fillMaxWidth().padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween){
+        Column(){
+            Text(name, )
+            Text("#${number}", modifier = Modifier.align(Alignment.End))
+        }
+        Box{
+            Image(painter = painterResource(R.drawable.pokeball),
+                contentDescription = "pokeball image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(130.dp).offset(30.dp, 20.dp)
+            )
+            Image(painter = painterResource(if(fav)R.drawable.star_filled else R.drawable.star_outline),
+                contentDescription = if(fav) " yellow star filled" else "yellow star outline",
+                modifier = Modifier.align(Alignment.TopEnd)
+            )
+        }
+    }
+}
+
+@Composable
+fun PokemonCard(name:String, weight: Float, height: Float, description: String, ability: String, image: Int){
+    Card{
+        Image(painter = painterResource(image), contentDescription=name)
     }
 }
 
